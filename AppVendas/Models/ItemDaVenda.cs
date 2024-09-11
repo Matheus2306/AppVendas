b0ed83d551630e1f1c7521a28f890d4b126996f7
Matheus2306 <Math2306.r@gmail.com>
2024-09-11 14:39:35 -0300

    add models itemvenda,venda e produto

---
+++ AppVendas/Models/ItemDaVenda.cs
@@ -0,0 +1,16 @@
+﻿namespace AppVendas.Models
+{
+    public class ItemDaVenda
+    {
+        public Guid ItemDavendaId { get; set; }
+        /*relação com a venda*/
+        public Guid VendaId { get; set; }
+        public Venda? Venda { get; set; }
+        /* relação com produto*/
+        public Guid ProdutoId { get; set; }
+        public Produto? Produto { get; set; }
+        public double QtdadeVendida { get; set; }
+        public double ValorTotalDoItem { get; set; }
+
+    }
+}
---
+++ AppVendas/Models/Venda.cs
@@ -0,0 +1,14 @@
+﻿namespace AppVendas.Models
+{
+    public class Venda
+    {
+        public string VendaId { get; set; }
+        public int NotaFiscal { get; set; }
+        public DateTime DataVenda { get; set; }
+        public double? TotalVenda { get; set; }
+        /* Referencia para a model cliente*/
+        public Guid ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+
+    }
+}
