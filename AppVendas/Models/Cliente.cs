473a6e5f6b1e2c42d0dd8452c7b1a4a6b0bb16c7
Matheus2306 <Math2306.r@gmail.com>
2024-09-11 14:18:48 -0300

    add model cliente

---
+++ AppVendas/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace AppVendas.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string ClienteNome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+    }
+}
